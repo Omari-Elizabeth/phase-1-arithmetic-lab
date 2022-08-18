9dfe6e8f06556ec976074553287516c75bccdff5
Omari-Elizabeth <elizabeth.omari@student.moringaschool.com>
2022-08-18 09:46:04 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = Math.sqrt(62);
+let num2 = Math.sqrt(62);
+var multiply = num1*num2;
 
-// Write your code here
+var random = Math.ceil(Math.random())+1;
+
+const max = Math.max(20, 15, 12, 10);
+
+let num3 = 16;
+let num4 = 6;
+var mod = num3%num4;
